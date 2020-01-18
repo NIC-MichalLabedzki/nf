5928e0f8599c68a5f667463972bd1997b07ef772
Michał Łabędzki <michal.tomasz.labedzki@gmail.com>
2020-01-18 17:14:01 +0100

    fix for win
    and some experiments...

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 setup(use_scm_version=True,
       install_requires=[
         'win10toast ; platform_system=="Windows"',
-        'paramiko ; platform_system=="Windows"'
+        'paramiko ; platform_system=="Windows"',
+        'psutil ; platform_system=="Windows"'
       ],
       extras_require={
         'optionals': [ 'paramiko', 'plyer', 'psutil' ]
