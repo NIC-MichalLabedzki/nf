4657a2c80291b54c1e9277d087c1cc060c00a619
Michał Łabędzki <michal.tomasz.labedzki@gmail.com>
2020-01-25 16:08:49 +0100

    tests: experiment #12 pythin3.9

--- tests/test_main.py
+++ tests/test_main.py
@@ -584,7 +584,10 @@ def test_main_module_all_mock_bad_import_backend(backend, python_version):
     for module_name in modules:
         module_backup[module_name] = sys.modules[module_name] if module_name in sys.modules else None
 
-        sys.modules[module_name] = None
+        if sys_version_info == (3,9) and module_name == 'shutil':
+            pass
+        else:
+            sys.modules[module_name] = None
 
     with pytest.raises(SystemExit) as exit_e:
         import nf
